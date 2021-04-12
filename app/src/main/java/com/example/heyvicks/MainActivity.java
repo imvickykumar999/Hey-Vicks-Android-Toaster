0dc3e4855d79f62558b707e6fe9962bdc51f363d
Vicky Kumar <imvickykumar999@gmail.com>
2021-04-12 23:05:15 +0530

    complete

--- app/src/main/java/com/example/heyvicks/MainActivity.java
+++ app/src/main/java/com/example/heyvicks/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.heyvicks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Hey Vicks", Toast.LENGTH_SHORT).show();
+
     }
 }
